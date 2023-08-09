6b9c7dc0da3292ee51d9f12623f06984418c8fe0
HP <HP@DISPERSER>
2023-08-10 02:01:36 +0300

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,405 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Label1 = New Label()
+        Label2 = New Label()
+        Label3 = New Label()
+        lblDateToday = New Label()
+        lblTimeToday = New Label()
+        Label4 = New Label()
+        txtNights = New TextBox()
+        Label5 = New Label()
+        txtNightlyCharge = New TextBox()
+        Label6 = New Label()
+        Label7 = New Label()
+        Label8 = New Label()
+        txtRoomService = New TextBox()
+        txtTelephone = New TextBox()
+        txtMisc = New TextBox()
+        Label9 = New Label()
+        Label10 = New Label()
+        Label11 = New Label()
+        Label12 = New Label()
+        Label13 = New Label()
+        lblRoomCharges = New Label()
+        lblAddCharges = New Label()
+        lblSubtotal = New Label()
+        lblTax = New Label()
+        lblTotal = New Label()
+        btnCalculate = New Button()
+        btnClear = New Button()
+        btnExit = New Button()
+        GroupBox1 = New GroupBox()
+        GroupBox2 = New GroupBox()
+        GroupBox3 = New GroupBox()
+        GroupBox1.SuspendLayout()
+        GroupBox2.SuspendLayout()
+        GroupBox3.SuspendLayout()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Font = New Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point)
+        Label1.Location = New Point(277, 9)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(175, 28)
+        Label1.TabIndex = 0
+        Label1.Text = "Highlander Hotel"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(541, 52)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(90, 20)
+        Label2.TabIndex = 1
+        Label2.Text = "Toda's Date:"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(564, 92)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(45, 20)
+        Label3.TabIndex = 3
+        Label3.Text = "Time:"
+        ' 
+        ' lblDateToday
+        ' 
+        lblDateToday.BorderStyle = BorderStyle.Fixed3D
+        lblDateToday.Location = New Point(637, 47)
+        lblDateToday.Name = "lblDateToday"
+        lblDateToday.Size = New Size(175, 25)
+        lblDateToday.TabIndex = 4
+        ' 
+        ' lblTimeToday
+        ' 
+        lblTimeToday.BorderStyle = BorderStyle.Fixed3D
+        lblTimeToday.Location = New Point(637, 92)
+        lblTimeToday.Name = "lblTimeToday"
+        lblTimeToday.Size = New Size(175, 25)
+        lblTimeToday.TabIndex = 5
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Location = New Point(55, 37)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(55, 20)
+        Label4.TabIndex = 6
+        Label4.Text = "Nights:"
+        ' 
+        ' txtNights
+        ' 
+        txtNights.Location = New Point(132, 34)
+        txtNights.Name = "txtNights"
+        txtNights.Size = New Size(125, 27)
+        txtNights.TabIndex = 7
+        ' 
+        ' Label5
+        ' 
+        Label5.AutoSize = True
+        Label5.Location = New Point(9, 89)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(117, 20)
+        Label5.TabIndex = 8
+        Label5.Text = "Nightly Charges:"
+        ' 
+        ' txtNightlyCharge
+        ' 
+        txtNightlyCharge.Location = New Point(132, 86)
+        txtNightlyCharge.Name = "txtNightlyCharge"
+        txtNightlyCharge.Size = New Size(125, 27)
+        txtNightlyCharge.TabIndex = 9
+        ' 
+        ' Label6
+        ' 
+        Label6.AutoSize = True
+        Label6.Location = New Point(13, 27)
+        Label6.Name = "Label6"
+        Label6.Size = New Size(103, 20)
+        Label6.TabIndex = 10
+        Label6.Text = "Room Service:"
+        ' 
+        ' Label7
+        ' 
+        Label7.AutoSize = True
+        Label7.Location = New Point(22, 62)
+        Label7.Name = "Label7"
+        Label7.Size = New Size(81, 20)
+        Label7.TabIndex = 11
+        Label7.Text = "Telephone:"
+        ' 
+        ' Label8
+        ' 
+        Label8.AutoSize = True
+        Label8.Location = New Point(52, 99)
+        Label8.Name = "Label8"
+        Label8.Size = New Size(42, 20)
+        Label8.TabIndex = 12
+        Label8.Text = "Misc:"
+        ' 
+        ' txtRoomService
+        ' 
+        txtRoomService.Location = New Point(127, 24)
+        txtRoomService.Name = "txtRoomService"
+        txtRoomService.Size = New Size(125, 27)
+        txtRoomService.TabIndex = 13
+        ' 
+        ' txtTelephone
+        ' 
+        txtTelephone.Location = New Point(127, 59)
+        txtTelephone.Name = "txtTelephone"
+        txtTelephone.Size = New Size(125, 27)
+        txtTelephone.TabIndex = 14
+        ' 
+        ' txtMisc
+        ' 
+        txtMisc.Location = New Point(127, 92)
+        txtMisc.Name = "txtMisc"
+        txtMisc.Size = New Size(125, 27)
+        txtMisc.TabIndex = 15
+        ' 
+        ' Label9
+        ' 
+        Label9.AutoSize = True
+        Label9.Location = New Point(35, 21)
+        Label9.Name = "Label9"
+        Label9.Size = New Size(109, 20)
+        Label9.TabIndex = 16
+        Label9.Text = "Room Charges:"
+        ' 
+        ' Label10
+        ' 
+        Label10.AutoSize = True
+        Label10.Location = New Point(7, 60)
+        Label10.Name = "Label10"
+        Label10.Size = New Size(139, 20)
+        Label10.TabIndex = 17
+        Label10.Text = "Additional Charges:"
+        ' 
+        ' Label11
+        ' 
+        Label11.AutoSize = True
+        Label11.Location = New Point(66, 99)
+        Label11.Name = "Label11"
+        Label11.Size = New Size(68, 20)
+        Label11.TabIndex = 18
+        Label11.Text = "Subtotal:"
+        ' 
+        ' Label12
+        ' 
+        Label12.AutoSize = True
+        Label12.Location = New Point(101, 137)
+        Label12.Name = "Label12"
+        Label12.Size = New Size(33, 20)
+        Label12.TabIndex = 19
+        Label12.Text = "Tax:"
+        ' 
+        ' Label13
+        ' 
+        Label13.AutoSize = True
+        Label13.Location = New Point(36, 174)
+        Label13.Name = "Label13"
+        Label13.Size = New Size(102, 20)
+        Label13.TabIndex = 20
+        Label13.Text = "Total Charges:"
+        ' 
+        ' lblRoomCharges
+        ' 
+        lblRoomCharges.BorderStyle = BorderStyle.FixedSingle
+        lblRoomCharges.Location = New Point(150, 20)
+        lblRoomCharges.Name = "lblRoomCharges"
+        lblRoomCharges.Size = New Size(143, 25)
+        lblRoomCharges.TabIndex = 21
+        ' 
+        ' lblAddCharges
+        ' 
+        lblAddCharges.BorderStyle = BorderStyle.FixedSingle
+        lblAddCharges.Location = New Point(150, 59)
+        lblAddCharges.Name = "lblAddCharges"
+        lblAddCharges.Size = New Size(143, 25)
+        lblAddCharges.TabIndex = 22
+        ' 
+        ' lblSubtotal
+        ' 
+        lblSubtotal.BorderStyle = BorderStyle.FixedSingle
+        lblSubtotal.Location = New Point(150, 98)
+        lblSubtotal.Name = "lblSubtotal"
+        lblSubtotal.Size = New Size(143, 25)
+        lblSubtotal.TabIndex = 23
+        ' 
+        ' lblTax
+        ' 
+        lblTax.BorderStyle = BorderStyle.FixedSingle
+        lblTax.Location = New Point(150, 136)
+        lblTax.Name = "lblTax"
+        lblTax.Size = New Size(143, 25)
+        lblTax.TabIndex = 24
+        ' 
+        ' lblTotal
+        ' 
+        lblTotal.BorderStyle = BorderStyle.FixedSingle
+        lblTotal.Location = New Point(150, 173)
+        lblTotal.Name = "lblTotal"
+        lblTotal.Size = New Size(143, 25)
+        lblTotal.TabIndex = 25
+        ' 
+        ' btnCalculate
+        ' 
+        btnCalculate.Location = New Point(186, 522)
+        btnCalculate.Name = "btnCalculate"
+        btnCalculate.Size = New Size(112, 48)
+        btnCalculate.TabIndex = 26
+        btnCalculate.Text = "Calculate " & vbCrLf & "Charges"
+        btnCalculate.UseVisualStyleBackColor = True
+        ' 
+        ' btnClear
+        ' 
+        btnClear.Location = New Point(399, 522)
+        btnClear.Name = "btnClear"
+        btnClear.Size = New Size(112, 48)
+        btnClear.TabIndex = 27
+        btnClear.Text = "&Clear"
+        btnClear.UseVisualStyleBackColor = True
+        ' 
+        ' btnExit
+        ' 
+        btnExit.Location = New Point(621, 522)
+        btnExit.Name = "btnExit"
+        btnExit.Size = New Size(112, 48)
+        btnExit.TabIndex = 28
+        btnExit.Text = "Exit"
+        btnExit.UseVisualStyleBackColor = True
+        ' 
+        ' GroupBox1
+        ' 
+        GroupBox1.Controls.Add(txtNightlyCharge)
+        GroupBox1.Controls.Add(Label5)
+        GroupBox1.Controls.Add(txtNights)
+        GroupBox1.Controls.Add(Label4)
+        GroupBox1.Location = New Point(30, 146)
+        GroupBox1.Name = "GroupBox1"
+        GroupBox1.Size = New Size(297, 128)
+        GroupBox1.TabIndex = 29
+        GroupBox1.TabStop = False
+        GroupBox1.Text = "Room Information"
+        ' 
+        ' GroupBox2
+        ' 
+        GroupBox2.Controls.Add(txtMisc)
+        GroupBox2.Controls.Add(txtTelephone)
+        GroupBox2.Controls.Add(txtRoomService)
+        GroupBox2.Controls.Add(Label8)
+        GroupBox2.Controls.Add(Label7)
+        GroupBox2.Controls.Add(Label6)
+        GroupBox2.Location = New Point(533, 146)
+        GroupBox2.Name = "GroupBox2"
+        GroupBox2.Size = New Size(311, 128)
+        GroupBox2.TabIndex = 30
+        GroupBox2.TabStop = False
+        GroupBox2.Text = "Additional Charges"
+        ' 
+        ' GroupBox3
+        ' 
+        GroupBox3.Controls.Add(lblTotal)
+        GroupBox3.Controls.Add(lblTax)
+        GroupBox3.Controls.Add(lblSubtotal)
+        GroupBox3.Controls.Add(lblAddCharges)
+        GroupBox3.Controls.Add(lblRoomCharges)
+        GroupBox3.Controls.Add(Label13)
+        GroupBox3.Controls.Add(Label12)
+        GroupBox3.Controls.Add(Label11)
+        GroupBox3.Controls.Add(Label10)
+        GroupBox3.Controls.Add(Label9)
+        GroupBox3.Location = New Point(233, 291)
+        GroupBox3.Name = "GroupBox3"
+        GroupBox3.Size = New Size(376, 214)
+        GroupBox3.TabIndex = 31
+        GroupBox3.TabStop = False
+        GroupBox3.Text = "Total Charges"
+        ' 
+        ' Form1
+        ' 
+        AcceptButton = btnCalculate
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        CancelButton = btnExit
+        ClientSize = New Size(856, 582)
+        Controls.Add(GroupBox3)
+        Controls.Add(GroupBox2)
+        Controls.Add(GroupBox1)
+        Controls.Add(btnExit)
+        Controls.Add(btnClear)
+        Controls.Add(btnCalculate)
+        Controls.Add(lblTimeToday)
+        Controls.Add(lblDateToday)
+        Controls.Add(Label3)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Name = "Form1"
+        Text = "Form1"
+        GroupBox1.ResumeLayout(False)
+        GroupBox1.PerformLayout()
+        GroupBox2.ResumeLayout(False)
+        GroupBox2.PerformLayout()
+        GroupBox3.ResumeLayout(False)
+        GroupBox3.PerformLayout()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents lblDateToday As Label
+    Friend WithEvents lblTimeToday As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents txtNights As TextBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents txtNightlyCharge As TextBox
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents txtRoomService As TextBox
+    Friend WithEvents txtTelephone As TextBox
+    Friend WithEvents txtMisc As TextBox
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Label10 As Label
+    Friend WithEvents Label11 As Label
+    Friend WithEvents Label12 As Label
+    Friend WithEvents Label13 As Label
+    Friend WithEvents lblRoomCharges As Label
+    Friend WithEvents lblAddCharges As Label
+    Friend WithEvents lblSubtotal As Label
+    Friend WithEvents lblTax As Label
+    Friend WithEvents lblTotal As Label
+    Friend WithEvents btnCalculate As Button
+    Friend WithEvents btnClear As Button
+    Friend WithEvents btnExit As Button
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents GroupBox3 As GroupBox
+End Class
---
+++ Form1.vb
@@ -0,0 +1,86 @@
+﻿Public Class Form1
+    Private Sub Form_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+        ' Create a Date variable
+        Dim TodayDate As Date = DateTime.Now
+
+        lblDateToday.Text = TodayDate.ToString("D")
+
+        'create a time variable 
+        Dim CurrentTime As DateTime = DateTime.Now
+
+        lblTimeToday.Text = CurrentTime.ToString("T")
+
+
+
+    End Sub
+    Private Sub Label14_Click(sender As Object, e As EventArgs) Handles lblAddCharges.Click
+
+    End Sub
+
+    Private Sub btnCalculate_Click(sender As Object, e As EventArgs) Handles btnCalculate.Click
+        Dim decRoomCharges As Decimal, decAddCharges As Decimal, decSubTotal As Decimal
+        Dim decTax As Decimal, decTotal As Decimal, decNights As Decimal, decNightlyCharge As Decimal
+        Const decTAX_RATE As Double = 0.08
+
+        decNights = txtNights.Text
+        decNightlyCharge = txtNightlyCharge.Text
+
+        decRoomCharges = (decNights * decNightlyCharge)
+        lblRoomCharges.Text = decRoomCharges.ToString("C")
+
+        Dim dbRoomService As Double, dbTelephone As Double, dbMisc As Double
+        dbRoomService = CDec(txtRoomService.Text)
+        dbTelephone = CDec(txtTelephone.Text)
+        dbMisc = CDec(txtMisc.Text)
+
+        decAddCharges = dbRoomService + dbTelephone + dbMisc
+        lblAddCharges.Text = decAddCharges.ToString("C")
+
+        decSubTotal = decRoomCharges + decAddCharges
+        lblSubtotal.Text = decSubTotal.ToString("C")
+
+        decTax = decSubTotal * decTAX_RATE
+        lblTax.Text = decTax.ToString("C")
+
+        decTotal = decSubTotal + decTax
+        lblTotal.Text = decTotal.ToString("C")
+
+
+
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        txtNights.Clear()
+        txtNightlyCharge.Clear()
+        txtRoomService.Clear()
+        txtTelephone.Clear()
+        txtMisc.Clear()
+
+
+        lblRoomCharges.Text = " "
+        lblAddCharges.Text = " "
+        lblSubtotal.Text = " "
+        lblTax.Text = " "
+        lblTotal.Text = " "
+
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+
+    Private Sub lblDateToday_Click(sender As Object, e As EventArgs) Handles lblDateToday.Click
+
+
+    End Sub
+
+    Private Sub lblTimeToday_Click(sender As Object, e As EventArgs) Handles lblTimeToday.Click
+
+    End Sub
+
+    Private Sub GroupBox2_Enter(sender As Object, e As EventArgs) Handles GroupBox2.Enter
+
+    End Sub
+
+End Class
